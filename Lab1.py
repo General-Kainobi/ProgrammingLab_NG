364fbc0aef52b02b6e84ade0428711a26de223f4
General_Kainobi <natan.giassi@gmail.com>
2024-11-06 10:51:09 +0100

    Hello There

---
+++ Lab1.py
@@ -0,0 +1,15 @@
+import math 
+import random
+def main():
+    
+    print("Hello World \n ")
+    list = [3,7,69,-5,92]
+    for item in list:
+        if item<5:
+            print(item)
+        
+        
+
+
+if __name__ == '__main__':
+    main()
